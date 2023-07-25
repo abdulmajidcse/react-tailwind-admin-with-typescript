ee9e75fd953e0fdc6bca12bc81c9417c78f9e556
Abdul Majid <abdulmajidcse@gmail.com>
2023-07-25 20:53:05 +0600

    add .env file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <h2 className="text-xl">
-        Welcome to ReactJS + TS & TailwindCSS Admin Template
-      </h2>
+      <h2 className="text-xl">Welcome to {import.meta.env.VITE_APP_NAME}</h2>
       <Link
         to="/auth"
         className="text-blue-500 hover:underline hover:text-yellow-500"
